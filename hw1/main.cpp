635dbce610c4836a752d9cdf2b685b4b566bd6d8
Koren Ma'avari <koren130@gmail.com>
2024-11-29 15:33:36 +0200

    Add files via upload

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -7,166 +7,32 @@
 using namespace output;
 using namespace std;
 
-static const std::string token_names[] = {
-        "__FILLER_FOR_ZERO",
-        "VOID",
-        "INT",
-        "BYTE",
-        "BOOL",
-        "AND",
-        "OR",
-        "NOT",
-        "TRUE",
-        "FALSE",
-        "RETURN",
-        "IF",
-        "ELSE",
-        "WHILE",
-        "BREAK",
-        "CONTINUE",
-        "SC",
-        "COMMA",
-        "LPAREN",
-        "RPAREN",
-        "LBRACE",
-        "RBRACE",
-        "ASSIGN",
-        "RELOP",
-        "BINOP",
-        "COMMENT",
-        "ID",
-        "NUM",
-        "NUM_B",
-        "STRING"
-};
-
-// Function declarations for modularity
+// Function declarations
 void handleString(tokentype token);
 string handleEscapeSequence(string* string_mode, int* index);
 void handleInvalidEscapeSequence();
 void handleInvalidHexSequence();
+
+// Global variable for string tokens from scanner.lex
+extern std::string yylval_string;
 
 int main() {
     enum tokentype token;
 
     // Read tokens until the end of file is reached
     while ((token = static_cast<tokentype>(yylex())) != 0) {
-        // Print token details using the format: <line_number> <token_name> <value>
         switch (token) {
-            case VOID:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case INT:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case BYTE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case BOOL:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case AND:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case OR:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case NOT:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case TRUE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case FALSE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case RETURN:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case IF:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case ELSE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case WHILE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case BREAK:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case CONTINUE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case SC:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case COMMA:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case LPAREN:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case RPAREN:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case LBRACE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case RBRACE:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case ASSIGN:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case RELOP:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case BINOP:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case COMMENT:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case ID:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case NUM:
-                printToken(yylineno, token, yytext);
-                break;
-
-            case NUM_B:
+            case VOID: case INT: case BYTE: case BOOL: case AND:
+            case OR: case NOT: case TRUE: case FALSE: case RETURN:
+            case IF: case ELSE: case WHILE: case BREAK: case CONTINUE:
+            case SC: case COMMA: case LPAREN: case RPAREN:
+            case LBRACE: case RBRACE: case ASSIGN: case RELOP:
+            case BINOP: case COMMENT: case ID: case NUM: case NUM_B:
                 printToken(yylineno, token, yytext);
                 break;
 
             case STRING:
-                handleString(token); // Process string tokens separately
+                handleString(token);
                 break;
 
             default:
@@ -178,85 +44,57 @@ int main() {
     return 0;
 }
 
-// Handle string tokens and process escape sequences
+// Handle string tokens
 void handleString(tokentype token) {
-    string string_mode = string(yytext); // Convert the string token to a C++ string
-    // string string_mode(yytext); // Convert the string token to a C++ string
-    string processed_string = "";
+    std::string raw_string = yylval_string; // Get the raw string from scanner.lex
+    std::string processed_string;
+
     int i = 0;
-    int length = string_mode.size();
-    while (*yytext != '\0') {
-        cout << "@@@ yytext[i] = " << *yytext << " @@@" << endl;
-        //cout << "@@@ yytext[i+1] = " << yytext[i+1] << " @@@" << endl;
-        //cout << "@@@ yytext[i+2] = " << yytext[i+2] << " @@@" << endl;
-        yytext++;
-    }
-
-    // Iterate through the string to process escape sequences
-    while (yytext[i] != '\0') {
-    // while (i < length) {
-        cout << "@@@ The string is: " << string_mode << " @@@" << endl; 
-        cout << "@@@ " << i << " < " << length << " @@@" << endl;
-        if (string_mode[i] == '\\' && i + 1 < length) {
-            processed_string += handleEscapeSequence(&string_mode, &i); // Handle escape sequence
-        } else if (string_mode[i] == '"') {
-            cout << "@@@ manual debug @@@" << endl;
-            i++; // Skip quotation marks
-        } else {
-            processed_string += string_mode[i];
+    while (i < raw_string.size()) {
+        if (raw_string[i] == '\\') {
+            // Handle escape sequences
+            processed_string += handleEscapeSequence(&raw_string, &i);
             i++;
+        } else {
+            // Append regular characters
+            processed_string += raw_string[i];
         }
+        i++;
     }
 
-    printToken(yylineno, token, yytext);
+    printToken(yylineno, token, processed_string.c_str());
 }
 
-// Process valid escape sequences and handle invalid ones
+// Handle escape sequences within strings
 string handleEscapeSequence(string* string_mode, int* index) {
     char next_char = (*string_mode)[*index + 1];
 
     switch (next_char) {
-        case 'n':
-            return "\n";
-            break;
+        case 'n': return "\n";
+        case 'r': return "\r";
+        case 't': return "\t";
+        case '\\': return "\\";
+        case '\"': return "\"";
 
-        case 'r':
-            return "\r";
-            break;
-
-        case 't':
-            return "\t";
-            break;
-
-        case '\\':
-            return "\\";
-            break;
-            
-        case '\"':
-            return "\"";
-            break;
-            
         case 'x': {
-            // Handle hexadecimal escape sequence (\xNN)
-            int length = (*string_mode).size();
+            int length = string_mode->size();
             if (*index + 3 < length && isxdigit((*string_mode)[*index + 2]) && isxdigit((*string_mode)[*index + 3])) {
-                string hex_value = (*string_mode).substr(*index + 2, 2);
+                string hex_value = string_mode->substr(*index + 2, 2);
                 char hex_char = stoi(hex_value, nullptr, 16);
-                if (0x20 <= hex_char <= 0x21 ||
-                    0x23 <= hex_char <= 0x5B ||
-                    0x5D <= hex_char <= 0xFE) {
-                    *index += 3; // Advance past \xNN
+                if (hex_char >= 32 && hex_char <= 126) {
+                    *index += 3;
                     return string(1, hex_char);
                 }
             }
-            handleInvalidHexSequence(); // Invalid or incomplete hex escape
+            handleInvalidHexSequence();
             break;
         }
-        
+
         default:
-            handleInvalidEscapeSequence(); // Undefined escape sequence
+            handleInvalidEscapeSequence();
             break;
     }
+    return "";
 }
 
 // Handle invalid escape sequences
--- hw1/scanner.lex
+++ hw1/scanner.lex
@@ -1,27 +1,31 @@
 %{
-
 #include "tokens.hpp"
 #include "output.hpp"
+#include <string>
+#include <iostream>
+#include <sstream>
+
+std::string yylval_string; // Global variable to store string token
 
 void report_string_error(const char* msg);
-
+void pass_string_to_main(const char* str);
+std::string get_and_clear_string_accumulator();
 %}
 
 %option noyywrap
 %option yylineno
 
-DIGITS                 [0-9]                         
-LETTERS                [a-zA-Z]                      
-ALNUMS                 ([a-zA-Z0-9])                 
-WHITESPACES            [ \t\r\n]                     
-PRINTABLE              [\x20-\x21\x23-\x5B\x5D-\x7E] 
-ESCAPE_SEQUENCES       (\\[ntr\"0])                  
-HEX_ESCAPES            (\\x[0-9A-Fa-f]{2})           
+DIGITS                 [0-9]
+LETTERS                [a-zA-Z]
+ALNUMS                 ([a-zA-Z0-9])
+WHITESPACES            [ \t\r\n]
+PRINTABLE              [\x20-\x21\x23-\x5B\x5D-\x7E]
+ESCAPE_SEQUENCES       (\\[ntr\"0])
+HEX_ESCAPES            (\\x[0-9A-Fa-f]{2})
 
-%x STRING_MODE 
+%x STRING_MODE
 
 %%
-
 
 "void"                 { return VOID; }
 "int"                  { return INT; }
@@ -39,7 +43,6 @@ HEX_ESCAPES            (\\x[0-9A-Fa-f]{2})
 "break"                { return BREAK; }
 "continue"             { return CONTINUE; }
 
-
 ";"                    { return SC; }
 ","                    { return COMMA; }
 "("                    { return LPAREN; }
@@ -51,40 +54,52 @@ HEX_ESCAPES            (\\x[0-9A-Fa-f]{2})
 "<"|">"                { return RELOP; }
 "+"|"-"|"*"|"/"        { return BINOP; }
 
-
 \/\/[^\n\r]*           { return COMMENT; }
-
 
 {LETTERS}({ALNUMS})*   { return ID; }
 "0"                    { return NUM; }
 [1-9]+[0-9]*           { return NUM; }
-
-
 "0[bB]"                { return NUM_B; }
 [1-9]+[0-9]*[bB]       { return NUM_B; }
 
-
-\"                     { BEGIN(STRING_MODE); } 
+\"                     { BEGIN(STRING_MODE); }
 
 <STRING_MODE>{
-    {ESCAPE_SEQUENCES} {  }
-    {HEX_ESCAPES}      {  }
-    {PRINTABLE}        {  }
+    {ESCAPE_SEQUENCES} { pass_string_to_main(yytext); }
+    {HEX_ESCAPES}      { pass_string_to_main(yytext); }
+    {PRINTABLE}        { pass_string_to_main(yytext); }
     \\[^ntr\"0x]       { report_string_error("Invalid escape sequence"); }
     \\x[0-9A-Fa-f]?    { report_string_error("Malformed hex escape"); }
     \n                 { report_string_error("Unclosed string"); }
-    \"                 { BEGIN(INITIAL); return STRING; } /* End of string */
+    \r                 { report_string_error("Unclosed string"); }
+    \r\n               { report_string_error("Unclosed string"); }
+    \"                 { 
+        BEGIN(INITIAL); 
+        yylval_string = get_and_clear_string_accumulator(); // Use a global variable to store the string
+        return STRING;
+    }
 }
-
 
 {WHITESPACES}+         {  }
 \n                     { yylineno++; }
-
+\r\n                   { yylineno++; }
+\r                     { yylineno++; }
 
 .                      { output::errorUnknownChar(yytext[0]); }
 
 %%
 
+std::string string_accumulator;
+
+void pass_string_to_main(const char* str) {
+    string_accumulator += str;
+}
+
+std::string get_and_clear_string_accumulator() {
+    std::string full_string = string_accumulator;
+    string_accumulator.clear();
+    return full_string;
+}
 
 void report_string_error(const char* msg) {
     if (strcmp(msg, "Unclosed string") == 0) {
